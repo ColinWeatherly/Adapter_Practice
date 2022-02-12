a60dda580e09f8557dd35f7750a300bfe1a4710f
ColinWeatherly <colrweat@ut.utm.edu>
2022-02-11 19:25:22 -0600

    Updated each file in the program to include descriptions at the top

--- Adapter_Practice/Bear.cs
+++ Adapter_Practice/Bear.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/* Name: Colin Weatherly
+ * Date: 2/11/22
+ * File: Bear.cs
+ * IDE: Visual Studio 2019
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
